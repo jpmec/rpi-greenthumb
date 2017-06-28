da91a92468359d4d6666690536859f76087e4632
Josh Petitt <joshpetitt@jpmec.com>
2017-06-27 21:08:45 -0500

    Initial commit of empty stub files for python module

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='rpi-greenthumb',
+    version='0.0.0',
+    description='Raspberry Pi greenthumb module',
+    url='https://github.com/jpmec/rpi-greenthumb',
+    author='Joshua Petitt',
+    license='MIT',
+    packages=['rpi_greenthumb'],
+    zip_safe=False
+)
