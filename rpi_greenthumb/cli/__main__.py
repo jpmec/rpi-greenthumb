8dede624de93541d9187e961ed7b8bb84441b348
Josh Petitt <joshpetitt@jpmec.com>
2017-06-27 21:24:50 -0500

    say hello

---
+++ rpi_greenthumb/cli/__main__.py
@@ -0,0 +1,13 @@
+"""
+Main command-line interface program for rpi_greenthumb
+"""
+
+def main():
+    """
+    Main function
+    """
+    print('Hello world!')
+
+
+if __name__ == '__main__':
+    main()
